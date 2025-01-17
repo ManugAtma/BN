21d24cc569949236864b5545f0b63c885840aa25
Manug Atmacayan <m.atmacayan@gmx.net>
2025-01-17 14:30:13 +0100

    gg

--- tests/ueben/TestUeben.java
+++ tests/ueben/TestUeben.java
@@ -9,6 +9,34 @@ import java.net.Socket;
 public class TestUeben {
 
 
+
+
+    @Test
+    void klausur() throws IOException {
+        Socket socket = new Socket("141.45.154.136", 4444);
+        SimpleFileServerClient sfsClient = new SimpleFileServerClient(socket.getInputStream(), socket.getOutputStream(), "");
+        sfsClient.getFile("values.txt");
+
+        File file = new File("values.txt");
+        FileInputStream fis = new FileInputStream(file);
+        DataInputStream dis = new DataInputStream(fis);
+        long l = dis.readLong();
+        int i = dis.readInt();
+        System.out.println(l);
+        System.out.println(i);
+
+        File myfile = new File("ergebnis.txt");
+        FileOutputStream fos = new FileOutputStream(myfile);
+        DataOutputStream dos = new DataOutputStream(fos);
+        long sum = l + i;
+        dos.writeLong(sum);
+        dos.writeUTF("Manug Atmacayan");
+        dos.writeUTF("594526");
+
+        sfsClient.putFile("ergebnis.txt");
+
+    }
+
     @Test
     void practiceSendFile() throws IOException {
         File file = new File("uebungs_file.txt");
